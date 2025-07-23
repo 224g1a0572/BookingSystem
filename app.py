08ed890b5b90170fb2e84047de95648256558477
224g1a0572@srit.ac.in <224g1a0572@srit.ac.in>
2025-07-23 20:55:56 +0530

    Fix Flask port and Render start command

--- app.py
+++ app.py
@@ -118,4 +118,6 @@ def view_bookings():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    port = int(os.environ.get('PORT', 10000))  # Render requires this
+    app.run(host='0.0.0.0', port=port)
